6973f48968d4974f1b0a011240c53301e83d874a
Chase Deveraux Smith <chases832@gmail.com>
2019-08-14 16:06:23 +0000

    Automatically backed up by Learn

--- app/models/file.rb
+++ app/models/file.rb
@@ -1,1 +1,13 @@
 # create files for your ruby classes in this directory
+class Listing
+  def guests
+    # returns an array of all guests who have stayed at a listing
+  end 
+  def trip 
+      # - returns an array of all trips at a listing
+  end 
+  def trip_count
+      # - returns the number of trips that have been taken to that listing
+  end 
+  def 
+end 
